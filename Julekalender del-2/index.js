bf11fddadc01cf8dc073a3fdc309bc7c5a3c4e1e
BorgeNielsen <77321897+BorgeNielsen@users.noreply.github.com>
2021-12-06 15:51:43 +0100

    Add files via upload

--- Julekalender del-2/index.js
+++ Julekalender del-2/index.js
@@ -1,3 +1,22 @@
+var countDownDate = new Date("Dec 24, 2021 00:00:01").getTime();
+var JUL = setInterval(function() {
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+        
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days + ":"
+    document.getElementById("hours").innerHTML = hours + ":" 
+    document.getElementById("mins").innerHTML = minutes + ":" 
+    document.getElementById("secs").innerHTML = seconds + ""
+
+}, 1000)
+
+
+
 
 
 
